338f38d426dec3e0df9e8233a2fc5347fdf94c6b
Abhinav Goyal <abhinav621999goyal@gmail.com>
2024-01-10 15:15:38 +0530

    add ClassBasedComponent

---
+++ 10_classbasedcomponent/src/App.js
@@ -0,0 +1,15 @@
+//Functional Component
+  // function App() {
+  //   return (
+  //     <h1>Functional Component</h1>
+  //   );
+  // }
+//Class Component
+import React from "react";
+class App extends React.Component{
+  //Render is the function which return some JSX
+  render(){
+    return <h1>Class Component</h1>
+  }
+}
+export default App;
